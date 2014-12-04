1cb94c9c6f1a3b2cec4a52eadaf34d6a7e09890e
Pierre BARON <prbaron22@gmail.com>
2014-12-04 20:31:23 +0100

    ajout de la directive maxlengthtitle

---
+++ app/scripts/directives/maxlengthtitle.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name aratoappApp.directive:maxLengthTitle
+ * @description
+ * # maxLengthTitle
+ */
+angular.module('aratoappApp')
+    .directive('maxLengthTitle', maxLengthTitle);
+
+maxLengthTitle.$inject = [];
+function maxLengthTitle() {
+    var directive = {
+        require : 'ngModel',
+        restrict: 'A',
+        link    : postLink
+    };
+
+    return directive;
+
+    function postLink(scope, element, attrs, ngModelCtrl) {
+        scope.$watch(function () {
+            return ngModelCtrl.$modelValue;
+        }, checkLength);
+
+        function checkLength(newValue, oldValue) {
+            var tooLong = newValue && newValue.length > 20;
+            ngModelCtrl.$setValidity('maxLength', !tooLong);
+
+            if (tooLong) {
+                element.parents('.form-group').addClass('has-error');
+            }
+            else {
+                element.parents('.form-group').removeClass('has-error');
+            }
+        }
+    }
+}
