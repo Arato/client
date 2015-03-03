9fecd0ea8b9f7d264aa7a12b4cfd6abb43b4e417
Pierre BARON <prbaron22@gmail.com>
2015-03-03 18:38:11 +0100

    fix karma conf

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,7 +29,6 @@ module.exports = function (config) {
             'bower_components/angular-sanitize/angular-sanitize.js',
             'bower_components/angular-touch/angular-touch.js',
             'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
-            'bower_components/angular-ladda/dist/angular-ladda.min.js',
             'app/scripts/**/*.js',
             'test/mock/**/*.js',
             'test/spec/**/*.js'
